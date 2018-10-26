b38becf437f461355b14ff2bbd9cd4c60f171d5d
Carter Yagemann <yagemann@gatech.edu>
2018-10-26 09:21:26 -0400

    Begin to refactor code.

--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+#
+# Copyright 2018 Carter Yagemann
+#
+# This file is part of Barnum.
+#
+# Barnum is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Barnum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Barnum.  If not, see <https://www.gnu.org/licenses/>.
 
 import json
 from os import path, listdir, mkdir, getcwd
